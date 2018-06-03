2bc80b1cfddc2b4a2476cd94056b1d382e143b87
Caleb Habimana-Griffin <jgriff@Calebs-MacBook-Pro.local>
2018-06-03 16:42:22 -0400

    debugging

--- src/components/expand.jsx
+++ src/components/expand.jsx
@@ -5,6 +5,8 @@ import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Typography from '@material-ui/core/Typography';
+
 const styles = theme => ({
   heading: {
     fontSize: theme.typography.pxToRem(15),
